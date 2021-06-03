efd3f101b42487f5ecaeb3eeaab14400bc924026
Kim <36648257+kim-ras@users.noreply.github.com>
2021-06-03 12:57:57 +0200

    Minor change to the PowerScript

--- install/Create_local_db.ps1
+++ install/Create_local_db.ps1
@@ -1,4 +1,4 @@
 sqlcmd -S localhost -Q "create database to_do"
 sqlcmd -S localhost -Q "CREATE LOGIN dbuser WITH password='#S3cr3t!'"
-sqlcmd -S localhost -Q "USE to_do; GO; EXEC sp_addrolemember 'db_datareader', 'dbuser'"
+sqlcmd -S localhost -Q "USE to_do; EXEC sp_addrolemember 'db_datareader', 'dbuser'"
 sqlcmd -S localhost -i .\BuildToDoTables.sql
